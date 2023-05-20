0b7434d6626ea37de3d6d8f3ffcf0f47eb09da64
liurundong <liurundong@stu.scau.edu.cn>
2023-05-20 13:38:32 +0800

    [FIX] Fix video url extract failed
    Add `url = ret['page_info']['media_info']['stream_url']`

--- WeiboScrapy/util.py
+++ WeiboScrapy/util.py
@@ -45,26 +45,14 @@ def get_blog_item(ret: dict):
         'ts': int(time()),
     }
 
-    # text = json.dumps(ret, ensure_ascii=False)
-    # # If has video, get video url.
-    # video_re = re.search(r'"(http://f.video.weibocdn.com/.*?)"', text)
-    # if video_re:
-    #     item['video_url'] = video_re.group(1)
-    # # If has article, get article id.
-    # article_re = re.search(r'https://weibo.com/ttarticle/p/show\?id=(\d*)', text)
-    # if article_re:
-    #     item['article_id'] = article_re.group(1)
-
     if 'mix_media_info' in ret:
         for media_item in ret['mix_media_info']['items']:
             if media_item['type'] == 'pic':
                 item['img_count'] += 1
                 item['medias'][media_item['data']['largest']['url']] = 'img'
-                # item['file_urls'].append(media_item['data']['largest']['url'])
             elif media_item['type'] == 'video':
                 item['video_count'] += 1
                 item['medias'][media_item['data']['media_info']['mp4_720p_mp4']] = 'video'
-                # item['file_urls'].append(media_item['data']['media_info']['mp4_720p_mp4'])
             else:
                 logger.warning(f'mix_media_info item, not pic and video - {item["mblogid"] = }, {item["uid"] = }, {media_item.get("id", "") = }')
 
@@ -72,7 +60,6 @@ def get_blog_item(ret: dict):
         for pic in ret['pic_infos']:
             item['img_count'] += 1
             item['medias'][ret['pic_infos'][pic]['original']['url']] = 'img'
-            # item['file_urls'].append(ret['pic_infos'][pic]['original']['url'])
 
     if '查看图片' in ret['text'].split('//<'):
         soup = BeautifulSoup(ret['text'], 'html.parser')
@@ -81,7 +68,6 @@ def get_blog_item(ret: dict):
             if link.text == '查看图片':
                 item['img_count'] += 1
                 item['medias'][link['href']] = 'img'
-                # item['file_urls'].append(link['href'])
 
     if 'page_info' in ret:
         object_type = ret['page_info'].get('object_type', '')
@@ -89,43 +75,25 @@ def get_blog_item(ret: dict):
             item['video_count'] += 1
             if 'cards' in ret['page_info']:
                 item['medias'][ret['page_info']['cards'][0]['media_info']['stream_url']] = 'video'
-                # item['file_urls'].append(ret['page_info']['cards'][0]['media_info']['stream_url'])
             else:
                 url = ret['page_info']['media_info']['mp4_720p_mp4']
                 if not url:
                     url = ret['page_info']['media_info']['mp4_hd_url']
+                if not url:
+                    url = ret['page_info']['media_info']['stream_url']
                 item['medias'][url] = 'video'
-                # item['file_urls'].append(url)
 
         elif object_type == 'article':
             item['article_count'] += 1
             item['medias']['https://weibo.com/ttarticle/p/show?id=' + ret['page_info']['page_id']] = 'article'
-            # item['file_urls'].append('https://weibo.com/ttarticle/p/show?id=' + ret['page_info']['page_id'])
 
     if ret['pic_num'] != item['img_count']:
-        logger.warning(f'pic_nums and img_count not pare - {item["mblogid"] = }, {item["uid"] = }, {ret["pic_num"] = }, {item["img_count"] = }, {item["file_urls"]}')
+        logger.warning(f'pic_nums and img_count not pare - {item["mblogid"] = }, {item["uid"] = }, {ret["pic_num"] = }, {item["img_count"] = }, {item["medias"]}\n' +
+                       f'{ret["text"] = }')
+
 
     if '微博视频' in ret['text'] and not item['video_count']:
         logger.warning(f'text has "微博视频" but video_count = 0 - {item["mblogid"] = }, {item["uid"] = }')
-
-
-    # if item['pic_num']:
-    #     if ret.get('pic_infos', {}):
-    #         pic_infos = ret['pic_infos']
-    #         item['image_urls'] = [pic_infos[k]['largest']['url'] for k in pic_infos]
-    #     elif ret.get('mix_media_info', {}):
-    #         media_items = ret['mix_media_info']['items']
-    #         item['image_urls'] = [media_item['data']['largest']['url'] for media_item in media_items if media_item['type'] == 'pic']
-    #     elif '查看图片' in ret['text']:
-    #         soup = BeautifulSoup(ret['text'], 'html.parser')
-    #         links = soup.find_all('a')
-    #         for link in links:
-    #             if link.text == '查看图片':
-    #                 item['image_urls'].append(link['href'])
-    #     item['pic_ids'] = [re.search(r'.*/(\w*)', url).group(1) for url in item['image_urls']]
-
-    # if item['pic_num'] != len(item['image_urls']):
-    #     logger.info(f'pic_nums and collected urls not pare - {item["mblogid"] = }, {item["uid"] = }, {item["pic_num"] = }, {len(item["image_urls"]) = }')
 
     return item
 
